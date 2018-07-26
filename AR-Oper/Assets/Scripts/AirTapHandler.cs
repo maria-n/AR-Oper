2c2e4ef27c0cbc767f4a58f72a7aaf31321459c1
Maria N <maria-n@users.noreply.github.com>
2018-07-26 22:43:34 +0200

    Added AirTapHandler
    currently changes color needs to be set to toggle

---
+++ AR-Oper/Assets/Scripts/AirTapHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+
+public class AirTapHandler : Singleton<AirTapHandler> , IInputClickHandler {
+    private Color defaultColor;
+
+    // Use this for initialization
+    protected override void Awake()
+    {
+        defaultColor = gameObject.GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (gameObject.GetComponent<MeshRenderer>().material.color != Color.red)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = defaultColor;
+        }
+    }
+}
--- AR-Oper/Assets/Scripts/FocusHandler.cs
+++ AR-Oper/Assets/Scripts/FocusHandler.cs
@@ -22,13 +22,11 @@ public class FocusHandler : Singleton<FocusHandler> , IFocusable
     public void OnFocusEnter()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
-        Debug.Log("hit");
     }
 
     public void OnFocusExit ()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = defaultColor;
-        Debug.Log("no hit");
     }
 
 }
