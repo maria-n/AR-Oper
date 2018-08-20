3669d6fff2b5241896a220e3a9d7040333032876
Maria N <maria-n@users.noreply.github.com>
2018-08-20 17:19:11 +0200

    Stage platzieren und über "Stage" Keyword collider togglen

---
+++ AR-Oper/Assets/Scripts/SpeechManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechManagerScript : MonoBehaviour {
+    private static bool created = false;
+    public Collider stageCollider;
+    void Awake()
+    {
+        stageCollider = GetComponent<Collider>();
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toggleStage()
+    {
+        if(stageCollider.enabled)
+        {
+            stageCollider.enabled = !stageCollider.enabled;
+        }
+        else
+        {
+            stageCollider.enabled = true;
+        }
+    }
+}
